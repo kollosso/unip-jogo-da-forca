f7302a21c7ba648d88609bc6c19855527c8d22ec
Denis Medeiros <contato.denismedeiros@hotmail.com>
2021-05-04 21:11:08 -0300

    feat: config ejs engine

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const hangman = require('../hangman')
 
 const app = express()
+
+app.locals.hangman
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    hangman: hangman
+  })
 })
 
 app.listen(8080, () => {
