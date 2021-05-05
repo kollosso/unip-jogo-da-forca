abd84dc00cff32663d5e5323c86d3996d897589a
Kollosso <opticom.motorola@hotmail.com>
2021-05-05 10:07:56 -0300

    refactor: using static site
    remove ejs and use static file in express

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
 const express = require('express')
-const hangman = require('../hangman')
-
 const app = express()
 
-app.locals.hangman
-
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    hangman: hangman
-  })
-})
+app.use('/static', express.static('views'))
+app.use('/static', express.static('js'))
 
 app.listen(8080, () => {
   console.log('Server start on port, 8080')
